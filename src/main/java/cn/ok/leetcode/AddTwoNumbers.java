98bf13dfac0754073596b9644989a4e2b64f332f
Kyou <kyou_88@foxmail.com>
2024-02-05 16:57:26 +0800

    两数求和 - 第三种方实现法，实现绘制各实现方法每次计算耗时曲线图。

--- src/main/java/cn/ok/leetcode/AddTwoNumbers.java
+++ src/main/java/cn/ok/leetcode/AddTwoNumbers.java
@@ -2,10 +2,12 @@ package cn.ok.leetcode;
 
 import com.github.sh0nk.matplotlib4j.NumpyUtils;
 import com.github.sh0nk.matplotlib4j.Plot;
-import com.github.sh0nk.matplotlib4j.PlotImpl;
+import com.github.sh0nk.matplotlib4j.PythonConfig;
 import com.github.sh0nk.matplotlib4j.PythonExecutionException;
+import com.google.common.collect.Lists;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
@@ -14,24 +16,15 @@ import java.util.stream.Collectors;
 public class AddTwoNumbers {
     public static void main(String[] args) throws PythonExecutionException, IOException {
 
-        List<Double> x = NumpyUtils.linspace(-Math.PI, Math.PI, 256);
-        List<Double> C = x.stream().map(Math::cos).collect(Collectors.toList());
-        List<Double> S = x.stream().map(Math::sin).collect(Collectors.toList());
 
-        Plot plt = Plot.create();
-        plt.plot().add(x, C);
-        plt.plot().add(x, S);
-        plt.show();
 
 
 /*
-
         Random random = new Random();
 
         List<Integer> a1 = Arrays.stream(random.ints(0, 10).limit(100).toArray()).boxed().collect(Collectors.toList());
         List<Integer> a2 = Arrays.stream(random.ints(0, 10).limit(100).toArray()).boxed().collect(Collectors.toList());
 */
-
 
     }
 
--- src/main/java/cn/ok/leetcode/TwoSum.java
+++ src/main/java/cn/ok/leetcode/TwoSum.java
@@ -1,42 +1,80 @@
 package cn.ok.leetcode;
 
-import cn.ok.Util.TimeWatch;
+import com.github.sh0nk.matplotlib4j.Plot;
+import com.github.sh0nk.matplotlib4j.PythonExecutionException;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
+import java.io.IOException;
+import java.util.*;
 
 public class TwoSum {
+
+    private static final int RANDOM_MAX = 100000;
+    private static final int RANDOM_MIN = 0;
+    private final Random random = new Random();
+
     public static void main(String[] args) {
+        new TwoSum().test1();
+    }
 
-        Random random = new Random();
-        int max = 10000;
-        int min = -10000;
-        int[] nums = random.ints(min, max).limit(5000).toArray();
-        int[] targets = random.ints(min, max).limit(500).toArray();
+    /**
+     * 测试
+     */
+    private void test1() {
+        int numsSize = 5000;
+        int[] nums = random.ints(RANDOM_MIN, RANDOM_MAX).limit(numsSize).toArray();
 
-        try (TimeWatch ignored = new TimeWatch("方法二总")) {
+        int targetCnt = 500;
+        int[] targets = random.ints(RANDOM_MIN, RANDOM_MAX).limit(targetCnt).toArray();
 
-            for (int target : targets) {
-                try (TimeWatch ignored1 = new TimeWatch("twosum2")) {
-                    twosum2(nums, target);
-                }
-            }
+
+        List<Long> method1TimeUsed = new ArrayList<>(targets.length);
+        List<Long> method2TimeUsed = new ArrayList<>(targets.length);
+        List<Long> method3TimeUsed = new ArrayList<>(targets.length);
+
+        long t = System.nanoTime();
+        long u;
+        for (int target : targets) {
+            u = System.nanoTime();
+            twosum1(nums, target);
+            method1TimeUsed.add(System.nanoTime() - u);
         }
+        System.out.println("method1TimeUsedTotal: " + (System.nanoTime() - t));
 
-        System.gc();
+        t = System.nanoTime();
+        for (int target : targets) {
+            u = System.nanoTime();
+            twosum2(nums, target);
+            method2TimeUsed.add(System.nanoTime() - u);
+        }
+        System.out.println("method2TimeUsedTotal: " + (System.nanoTime() - t));
 
-        try (TimeWatch ignored = new TimeWatch("方法一总")) {
+        t = System.nanoTime();
+        for (int target : targets) {
+            u = System.nanoTime();
+            twosum3(nums, target);
+            method3TimeUsed.add(System.nanoTime() - u);
+        }
+        System.out.println("method3TimeUsedTotal: " + (System.nanoTime() - t));
 
-            for (int target : targets) {
-                try (TimeWatch ignored1 = new TimeWatch("twosum1")) {
-                    twosum1(nums, target);
-                }
-            }
+
+        Plot plot = Plot.create();
+        plot.plot().add(method1TimeUsed).label("Two For");
+        plot.plot().add(method2TimeUsed).label("Hash Left");
+        plot.plot().add(method3TimeUsed).label("Hash Left And Right");
+
+        plot.legend().loc("upper right");
+        plot.title("Nums Size: " + numsSize + " | Target Count: " + targetCnt);
+
+        plot.savefig("/Users/kyou/Desktop/" + numsSize + "_" + targetCnt + ".png").dpi(2000).format("png");
+
+        try {
+            plot.show();
+        } catch (IOException | PythonExecutionException e) {
+            throw new RuntimeException(e);
         }
     }
 
-    private static int[] twosum1(int[] nums, int target) {
+    private int[] twosum1(int[] nums, int target) {
 
         for (int i = 0; i < nums.length; i++) {
             int aim = target - nums[i];
@@ -51,7 +89,7 @@ public class TwoSum {
         return null;
     }
 
-    private static int[] twosum2(int[] nums, int target) {
+    private int[] twosum2(int[] nums, int target) {
 
         Map<Integer, Integer> map = new HashMap<>();
 
@@ -65,4 +103,24 @@ public class TwoSum {
 
         return null;
     }
+
+    private int[] twosum3(int[] nums, int target) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0, j = nums.length - 1; i <= j; i++, j--) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
+
+            if (map.containsKey(target - nums[j])) {
+                return new int[]{map.get(target - nums[j]), j};
+            }
+            map.put(nums[j], j);
+        }
+
+        return null;
+    }
+
 }
